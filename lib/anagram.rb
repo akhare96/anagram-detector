58a0a0f194b2bcf042092e5310670861a81c3b49
Ambar Khare <ambar.khare24@gmail.com>
2021-08-18 09:25:48 -0700

    commit tree 6d9036d5a354f96551e1b2181fefdf93ddcfc408

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        collection = []
+        array.each {|letter|
+        if letter.split("").sort == @word.split("").sort
+            collection << letter
+        end
+        }
+        collection
+    end
+end
